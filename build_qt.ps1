92230941daa8b923e135705c7d09ba91445103a0
Lupinzar <109043392+Lupinzar@users.noreply.github.com>
2024-11-16 16:59:25 -0800

    new resource + path fix

--- build_qt.ps1
+++ build_qt.ps1
@@ -1,4 +1,5 @@
 $PSDefaultParameterValues['Out-File:Encoding'] = 'ascii'
 
+pyside6-rcc qt\resource.qrc -o gssex\uibase\resource_rc.py
 pyside6-uic --from-imports qt\mainwindow.ui > gssex\uibase\mainwindow.py
-pyside6-rcc qt\resource.qrc -o gssex\resource_rc.py
+pyside6-uic --from-imports qt\tabpalette.ui > gssex\uibase\tabpalette.py
