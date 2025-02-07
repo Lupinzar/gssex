1e15d80f7477508b5208c8ea4db07f10ad9b2acd
Lupinzar <109043392+Lupinzar@users.noreply.github.com>
2025-02-07 11:12:02 -0800

    Tile Maps milestone
    * "Final" UI changes
    * Wire together UI with render layer
    * Save/copy buttons working

--- gssex/ui/tabtilemap.py
+++ gssex/ui/tabtilemap.py
@@ -1,7 +1,12 @@
 from .rendertab import RenderTab
+from .app import pil_to_qimage, pil_to_clipboard
 from ..uibase.tabtilemap import Ui_TabTileMap
-from ..static import Plane
-from PySide6.QtWidgets import QButtonGroup
+from ..static import Plane, Priority
+from ..render import MapRender
+from PySide6.QtWidgets import QButtonGroup, QLabel
+from PySide6.QtGui import QPixmap, QShortcut, QKeySequence
+from PIL import Image
+from os.path import basename
 
 class TabTileMap(RenderTab, Ui_TabTileMap):
     def __init__(self, **kwargs):
@@ -10,17 +15,154 @@ class TabTileMap(RenderTab, Ui_TabTileMap):
         self.setup_plane_combo()
         self.mode_group = QButtonGroup()
         self.priority_group = QButtonGroup()
+        self.renderer: MapRender|None = None
+        self.info_labels: list[QLabel] = [
+            self.info_hscroll_label,
+            self.info_vscroll_label,
+            self.info_scroll_size_label,
+            self.info_window_size_label
+        ]
+
+        self.clear_valids()
         
         self.mode_group.addButton(self.mode_map_radio)
         self.mode_group.addButton(self.mode_screen_button)
+        self.mode_group.addButton(self.mode_marks_button)
         self.mode_map_radio.setChecked(True)
 
         self.priority_group.addButton(self.priority_both_radio)
         self.priority_group.addButton(self.priority_high_radio)
         self.priority_group.addButton(self.priority_low_radio)
         self.priority_both_radio.setChecked(True)
+
+        self.fullRefresh.connect(self.do_full_refresh)
+        self.saveStateChanged.connect(self.do_full_refresh)
+        self.paletteSwapped.connect(self.draw_main)
+
+        self.plane_combo.currentIndexChanged.connect(self.draw_main)
+        self.mode_group.buttonClicked.connect(self.draw_main)
+        self.priority_group.buttonClicked.connect(self.draw_main)
+        self.save_button.clicked.connect(self.save_image)
+        self.copy_button.clicked.connect(self.copy_to_clipboard)
+
+        self.register_shortcuts()
     
     def setup_plane_combo(self):
         for k in range(0, len(Plane)):
             enum = Plane(k)
             self.plane_combo.addItem(enum.name.replace("_", " ").title())
+
+    def register_shortcuts(self):
+        QShortcut(QKeySequence("Ctrl+S"), self, lambda: self.save_image())
+        QShortcut(QKeySequence("Ctrl+C"), self, lambda: self.copy_to_clipboard())
+
+    def update_info(self):
+        vdp = self.app.savestate.vdp_registers
+        win_size = vdp.get_window_cell_size()
+        self.info_hscroll_label.setText(vdp.scroll_mode_h.name.title())
+        self.info_vscroll_label.setText(vdp.scroll_mode_v.name.title())
+        self.info_scroll_size_label.setText(f'{vdp.scroll_width * 8} x {vdp.scroll_height * vdp.tile_height}')
+        self.info_window_size_label.setText(f'{win_size[0] * 8} x {win_size[1] * vdp.tile_height}')
+        
+    def clear_valids(self):
+        self.main_label.clear()
+        for lbl in self.info_labels:
+            lbl.setText('-')
+
+    def do_full_refresh(self):
+        if not self.app.valid_file:
+            self.renderer = None
+            self.clear_valids()
+            return
+        self.renderer = MapRender(self.app.savestate)
+        self.update_info()
+        self.draw_main()
+
+    def draw_main(self):
+        if not self.app.valid_file:
+            return
+        pilimg = self.get_pil_image()
+        qimg = pil_to_qimage(pilimg)
+        self.main_label.setFixedSize(pilimg.width, pilimg.height)
+        self.scrollAreaWidgetContents.setFixedSize(pilimg.width, pilimg.height)
+        self.main_label.setPixmap(QPixmap.fromImage(qimg))
+        
+        pilimg.close()
+
+    def get_pil_image(self) -> Image.Image:
+        match(self.mode_group.checkedButton()):
+            case self.mode_screen_button:
+                return self.get_pil_screen()
+            case self.mode_marks_button:
+                return self.get_pil_marks()
+            case _:
+                return self.get_pil_map()
+            
+    def get_pil_screen(self) -> Image.Image:
+        pal_data = self.app.get_palette_and_background()
+        img = self.renderer.render_screen(
+            Plane(self.plane_combo.currentIndex()),
+            self.priority_ui_to_enum(),
+            pal_data[0]
+        )
+        img.putpalette(pal_data[1].flattened_colors())
+        return img
+    
+    def get_pil_marks(self) -> Image.Image:
+        raise Exception("not implemented yet")
+    
+    def get_pil_map(self) -> Image.Image:
+        pal_data = self.app.get_palette_and_background()
+        img = self.renderer.render_map(
+            Plane(self.plane_combo.currentIndex()),
+            self.priority_ui_to_enum(),
+            pal_data[0]
+        )
+        img.putpalette(pal_data[1].flattened_colors())
+        return img
+    
+    def save_image(self):
+        if not self.app.valid_file:
+            self.statusMessage.emit(self.STATE_NOT_VALID_MSG)
+            return
+        img = self.get_pil_image()
+        path = self.build_image_path()
+        if self.app.save_image(img, path):
+            self.statusMessage.emit(f"Output {path}")
+        else:
+            self.statusMessage.emit(f"Error outputting {path}")
+        img.close()
+
+    def copy_to_clipboard(self):
+        if not self.app.valid_file:
+            self.statusMessage.emit(self.STATE_NOT_VALID_MSG)
+            return
+        img = self.get_pil_image()
+        pil_to_clipboard(img)
+        img.close()
+
+    def build_image_path(self) -> str:
+        parts = [
+            self.plane_combo.currentText().lower().replace(" ", "-"),
+            self.mode_to_save_string(),
+            self.priority_ui_to_enum().name.lower(),
+            'global' if self.app.use_global_pal else 'state'
+        ]
+        key = "_".join(parts)
+        return self.app.build_image_output_path(key)
+
+    def priority_ui_to_enum(self) -> Priority:
+        match(self.priority_group.checkedButton()):
+            case self.priority_both_radio:
+                return Priority.BOTH
+            case self.priority_high_radio:
+                return Priority.HIGH
+        return Priority.LOW
+    
+    def mode_to_save_string(self) -> str:
+        match self.mode_group.checkedButton():
+            case self.mode_screen_button:
+                return 'screen'
+            case self.mode_marks_button:
+                return 'screen-marks'
+        return 'tilemap'
--- gssex/uibase/tabtilemap.py
+++ gssex/uibase/tabtilemap.py
@@ -15,7 +15,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QHBoxLayout,
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QHBoxLayout,
     QLabel, QPushButton, QRadioButton, QScrollArea,
     QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
 from . import resource_rc
@@ -24,7 +24,7 @@ class Ui_TabTileMap(object):
     def setupUi(self, TabTileMap):
         if not TabTileMap.objectName():
             TabTileMap.setObjectName(u"TabTileMap")
-        TabTileMap.resize(493, 436)
+        TabTileMap.resize(493, 476)
         self.horizontalLayout_3 = QHBoxLayout(TabTileMap)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.plane_scroll = QScrollArea(TabTileMap)
@@ -32,7 +32,7 @@ class Ui_TabTileMap(object):
         self.plane_scroll.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 297, 416))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 327, 456))
         self.main_label = QLabel(self.scrollAreaWidgetContents)
         self.main_label.setObjectName(u"main_label")
         self.main_label.setGeometry(QRect(0, 0, 81, 16))
@@ -62,22 +62,22 @@ class Ui_TabTileMap(object):
 
         self.verticalLayout_2.addWidget(self.label_5)
 
-        self.horizontalLayout_4 = QHBoxLayout()
-        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
         self.mode_map_radio = QRadioButton(TabTileMap)
         self.mode_map_radio.setObjectName(u"mode_map_radio")
         self.mode_map_radio.setChecked(True)
 
-        self.horizontalLayout_4.addWidget(self.mode_map_radio)
+        self.verticalLayout_2.addWidget(self.mode_map_radio)
 
         self.mode_screen_button = QRadioButton(TabTileMap)
         self.mode_screen_button.setObjectName(u"mode_screen_button")
         self.mode_screen_button.setChecked(False)
 
-        self.horizontalLayout_4.addWidget(self.mode_screen_button)
+        self.verticalLayout_2.addWidget(self.mode_screen_button)
 
+        self.mode_marks_button = QRadioButton(TabTileMap)
+        self.mode_marks_button.setObjectName(u"mode_marks_button")
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
+        self.verticalLayout_2.addWidget(self.mode_marks_button)
 
         self.label_4 = QLabel(TabTileMap)
         self.label_4.setObjectName(u"label_4")
@@ -100,71 +100,88 @@ class Ui_TabTileMap(object):
 
         self.verticalLayout_2.addWidget(self.priority_low_radio)
 
-        self.line = QFrame(TabTileMap)
-        self.line.setObjectName(u"line")
-        self.line.setFrameShape(QFrame.Shape.HLine)
-        self.line.setFrameShadow(QFrame.Shadow.Sunken)
+        self.save_button = QPushButton(TabTileMap)
+        self.save_button.setObjectName(u"save_button")
+        icon = QIcon()
+        icon.addFile(u":/icons/device-floppy.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.save_button.setIcon(icon)
 
-        self.verticalLayout_2.addWidget(self.line)
+        self.verticalLayout_2.addWidget(self.save_button)
+
+        self.copy_button = QPushButton(TabTileMap)
+        self.copy_button.setObjectName(u"copy_button")
+        icon1 = QIcon()
+        icon1.addFile(u":/icons/clipboard.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.copy_button.setIcon(icon1)
+
+        self.verticalLayout_2.addWidget(self.copy_button)
+
+        self._info_label = QLabel(TabTileMap)
+        self._info_label.setObjectName(u"_info_label")
+
+        self.verticalLayout_2.addWidget(self._info_label)
+
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.label_3 = QLabel(TabTileMap)
+        self.label_3.setObjectName(u"label_3")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy1)
+
+        self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
+
+        self.info_hscroll_label = QLabel(TabTileMap)
+        self.info_hscroll_label.setObjectName(u"info_hscroll_label")
+        sizePolicy.setHeightForWidth(self.info_hscroll_label.sizePolicy().hasHeightForWidth())
+        self.info_hscroll_label.setSizePolicy(sizePolicy)
+
+        self.gridLayout.addWidget(self.info_hscroll_label, 0, 1, 1, 1)
+
+        self.info_vscroll_label = QLabel(TabTileMap)
+        self.info_vscroll_label.setObjectName(u"info_vscroll_label")
+        sizePolicy.setHeightForWidth(self.info_vscroll_label.sizePolicy().hasHeightForWidth())
+        self.info_vscroll_label.setSizePolicy(sizePolicy)
+
+        self.gridLayout.addWidget(self.info_vscroll_label, 1, 1, 1, 1)
+
+        self.info_scroll_size_label = QLabel(TabTileMap)
+        self.info_scroll_size_label.setObjectName(u"info_scroll_size_label")
+        sizePolicy.setHeightForWidth(self.info_scroll_size_label.sizePolicy().hasHeightForWidth())
+        self.info_scroll_size_label.setSizePolicy(sizePolicy)
+
+        self.gridLayout.addWidget(self.info_scroll_size_label, 2, 1, 1, 1)
 
         self.label_2 = QLabel(TabTileMap)
         self.label_2.setObjectName(u"label_2")
-        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
-        self.label_2.setSizePolicy(sizePolicy)
+        sizePolicy1.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy1)
 
-        self.verticalLayout_2.addWidget(self.label_2)
+        self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
 
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.tile_map_save_button = QPushButton(TabTileMap)
-        self.tile_map_save_button.setObjectName(u"tile_map_save_button")
-        icon = QIcon()
-        icon.addFile(u":/icons/device-floppy.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.tile_map_save_button.setIcon(icon)
+        self.label_6 = QLabel(TabTileMap)
+        self.label_6.setObjectName(u"label_6")
+        sizePolicy1.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
+        self.label_6.setSizePolicy(sizePolicy1)
 
-        self.horizontalLayout.addWidget(self.tile_map_save_button)
+        self.gridLayout.addWidget(self.label_6, 2, 0, 1, 1)
 
-        self.tile_map_copy_button = QPushButton(TabTileMap)
-        self.tile_map_copy_button.setObjectName(u"tile_map_copy_button")
-        icon1 = QIcon()
-        icon1.addFile(u":/icons/clipboard.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.tile_map_copy_button.setIcon(icon1)
+        self.label_7 = QLabel(TabTileMap)
+        self.label_7.setObjectName(u"label_7")
 
-        self.horizontalLayout.addWidget(self.tile_map_copy_button)
+        self.gridLayout.addWidget(self.label_7, 3, 0, 1, 1)
 
+        self.info_window_size_label = QLabel(TabTileMap)
+        self.info_window_size_label.setObjectName(u"info_window_size_label")
+        sizePolicy.setHeightForWidth(self.info_window_size_label.sizePolicy().hasHeightForWidth())
+        self.info_window_size_label.setSizePolicy(sizePolicy)
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
-
-        self.line_2 = QFrame(TabTileMap)
-        self.line_2.setObjectName(u"line_2")
-        self.line_2.setFrameShape(QFrame.Shape.HLine)
-        self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
-
-        self.verticalLayout_2.addWidget(self.line_2)
-
-        self.label_3 = QLabel(TabTileMap)
-        self.label_3.setObjectName(u"label_3")
-        sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
-        self.label_3.setSizePolicy(sizePolicy)
-
-        self.verticalLayout_2.addWidget(self.label_3)
-
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.screen_save_button = QPushButton(TabTileMap)
-        self.screen_save_button.setObjectName(u"screen_save_button")
-        self.screen_save_button.setIcon(icon)
-
-        self.horizontalLayout_2.addWidget(self.screen_save_button)
-
-        self.screen_copy_button = QPushButton(TabTileMap)
-        self.screen_copy_button.setObjectName(u"screen_copy_button")
-        self.screen_copy_button.setIcon(icon1)
-
-        self.horizontalLayout_2.addWidget(self.screen_copy_button)
+        self.gridLayout.addWidget(self.info_window_size_label, 3, 1, 1, 1)
 
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.verticalLayout_2.addLayout(self.gridLayout)
 
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
@@ -186,15 +203,21 @@ class Ui_TabTileMap(object):
         self.label_5.setText(QCoreApplication.translate("TabTileMap", u"Render Mode", None))
         self.mode_map_radio.setText(QCoreApplication.translate("TabTileMap", u"Map", None))
         self.mode_screen_button.setText(QCoreApplication.translate("TabTileMap", u"Screen", None))
+        self.mode_marks_button.setText(QCoreApplication.translate("TabTileMap", u"Screen + Scroll Marks", None))
         self.label_4.setText(QCoreApplication.translate("TabTileMap", u"Priority", None))
         self.priority_both_radio.setText(QCoreApplication.translate("TabTileMap", u"Both", None))
         self.priority_high_radio.setText(QCoreApplication.translate("TabTileMap", u"High", None))
         self.priority_low_radio.setText(QCoreApplication.translate("TabTileMap", u"Low", None))
-        self.label_2.setText(QCoreApplication.translate("TabTileMap", u"Tile Map", None))
-        self.tile_map_save_button.setText(QCoreApplication.translate("TabTileMap", u"Save", None))
-        self.tile_map_copy_button.setText(QCoreApplication.translate("TabTileMap", u"Copy", None))
-        self.label_3.setText(QCoreApplication.translate("TabTileMap", u"Screen", None))
-        self.screen_save_button.setText(QCoreApplication.translate("TabTileMap", u"Save", None))
-        self.screen_copy_button.setText(QCoreApplication.translate("TabTileMap", u"Copy", None))
+        self.save_button.setText(QCoreApplication.translate("TabTileMap", u"Save", None))
+        self.copy_button.setText(QCoreApplication.translate("TabTileMap", u"Copy", None))
+        self._info_label.setText(QCoreApplication.translate("TabTileMap", u"Info", None))
+        self.label_3.setText(QCoreApplication.translate("TabTileMap", u"V Scroll", None))
+        self.info_hscroll_label.setText(QCoreApplication.translate("TabTileMap", u"-", None))
+        self.info_vscroll_label.setText(QCoreApplication.translate("TabTileMap", u"-", None))
+        self.info_scroll_size_label.setText(QCoreApplication.translate("TabTileMap", u"-", None))
+        self.label_2.setText(QCoreApplication.translate("TabTileMap", u"H Scroll", None))
+        self.label_6.setText(QCoreApplication.translate("TabTileMap", u"Scroll Size  ", None))
+        self.label_7.setText(QCoreApplication.translate("TabTileMap", u"Win. Size ", None))
+        self.info_window_size_label.setText(QCoreApplication.translate("TabTileMap", u"-", None))
     # retranslateUi
 
